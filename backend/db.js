ce069a57c8992276fdf94d5e4aea17d70f5d95a7
Sania <saniashanmugan@gmail.com>
2026-05-11 17:15:16 +0530

    Fixed DB port connection

--- backend/db.js
+++ backend/db.js
@@ -6,7 +6,10 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  timezone: "Z", 
+  port: process.env.DB_PORT,
+  timezone: "Z",
+  waitForConnections: true,
+  connectionLimit: 10,
 });
 
 module.exports = pool;
